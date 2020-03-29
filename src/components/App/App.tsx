f445f2a4acfd297ea37354910f8300b52ce4c61b
Ting Chen <ting@squareup.com>
2020-03-29 16:55:40 -0400

    Render merchant banner

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import MerchantPage from '../MerchantPage';
 import styles from './App.module.scss';
 
 function App() {
   return (
     <div className={styles.container}>
-      HELLO
+      <MerchantPage />
     </div>
   );
 }
---
+++ src/components/MerchantPage/MerchantPage.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import HeroBanner from '../HeroBanner';
+
+const MerchantPage: React.SFC = () => {
+  return (
+    <main>
+      <HeroBanner />
+    </main>
+  );
+}
+
+export default MerchantPage;
