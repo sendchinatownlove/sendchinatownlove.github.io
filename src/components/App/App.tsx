5a0bdbd593e972fa2b3898db37f2cfa55cffacec
Ting Chen <ting@squareup.com>
2020-03-29 16:29:39 -0400

    Start app from Create React App with TypeScript. Using SCSS Modules

---
+++ src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './App.module.scss';
+
+function App() {
+  return (
+    <div className={styles.container}>
+      HELLO
+    </div>
+  );
+}
+
+export default App;
