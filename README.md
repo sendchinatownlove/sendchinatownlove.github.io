b4a66709c75268f3ebed73482c4e79ef2792107a
Ting Chen <ting@squareup.com>
2020-03-29 21:25:11 -0400

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# sendchinatownlove.github.io
---
+++ precache-manifest.a3c72f5b83ab8b6a5f4772e30423614f.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b2011aaf69f5d1c73159212528d597",
+    "url": "/index.html"
+  },
+  {
+    "revision": "20771bc0596854d83a32",
+    "url": "/static/css/main.4976bb83.chunk.css"
+  },
+  {
+    "revision": "ea5fd7fedfc348ad1561",
+    "url": "/static/js/2.306f26c7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.306f26c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "20771bc0596854d83a32",
+    "url": "/static/js/main.47953601.chunk.js"
+  },
+  {
+    "revision": "b991e9c21dba036bc800",
+    "url": "/static/js/runtime-main.f7beece5.js"
+  },
+  {
+    "revision": "edd78b0db00c6af7c0b8b71ecee22b1a",
+    "url": "/static/media/logo.edd78b0d.png"
+  },
+  {
+    "revision": "f7ad639e27c869f0d9e8a62b484e7c13",
+    "url": "/static/media/merchant-front.f7ad639e.png"
+  },
+  {
+    "revision": "18cdfea7b54f5b5b2024068d16aaeb7c",
+    "url": "/static/media/sample-menu-item.18cdfea7.png"
+  }
+]);
