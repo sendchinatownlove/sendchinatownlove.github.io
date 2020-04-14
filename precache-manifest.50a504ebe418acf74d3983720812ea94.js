d9ad5d959848a239885046b20622b3f105b23af7
Justin McKibben <justintmckibben@gmail.com>
2020-04-14 15:41:41 -0500

    Updates

--- precache-manifest.50a504ebe418acf74d3983720812ea94.js
+++ precache-manifest.50a504ebe418acf74d3983720812ea94.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "2770e6cd3c0dce62b157535914d88414",
+    "revision": "16b2015f8e8f56aa85065fd7c94b2b77",
     "url": "/index.html"
   },
   {
-    "revision": "c0e3543270783e96c789",
-    "url": "/static/css/main.41d926b7.chunk.css"
+    "revision": "bc1cbd2d1dd9c012fc81",
+    "url": "/static/css/main.45e4df16.chunk.css"
   },
   {
-    "revision": "c7e269e5219a2281ba49",
-    "url": "/static/js/2.9a155418.chunk.js"
+    "revision": "50c2bda99a04efab2d76",
+    "url": "/static/js/2.015ef660.chunk.js"
   },
   {
     "revision": "3453b8997016469371284a28c0e873e2",
-    "url": "/static/js/2.9a155418.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.015ef660.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "c0e3543270783e96c789",
-    "url": "/static/js/main.bedef09d.chunk.js"
+    "revision": "bc1cbd2d1dd9c012fc81",
+    "url": "/static/js/main.98c711e9.chunk.js"
   },
   {
     "revision": "7ae0be752f21e964cf96",
@@ -34,6 +34,10 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "67dd739116e104ca62265f818c797ad2",
     "url": "/static/media/chinatown-splash.67dd7391.png"
+  },
+  {
+    "revision": "46412c79a0082e73eb711911bf68237d",
+    "url": "/static/media/female.46412c79.svg"
   },
   {
     "revision": "909f1479b519e732b5a828cc5a69317a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e10ccb28d586fe17fce869cd4669c4e4.js"
+  "/precache-manifest.50a504ebe418acf74d3983720812ea94.js"
 );
 
 self.addEventListener('message', (event) => {
