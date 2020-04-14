75374074b4cc742d9562bf1cc9e434d7cb6a9746
Justin McKibben <justintmckibben@gmail.com>
2020-04-13 19:09:28 -0500

    Run yarn format

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -10,8 +10,8 @@ const Home: React.SFC = () => {
         <div className={styles.introContainer}>
           <h1 className={styles.headerText}>Chinatown needs your help</h1>
           <p className={styles.introText}>
-            Support local Chinatown businesses during the COVID-19 pandemic
-            and help us bring their stores online
+            Support local Chinatown businesses during the COVID-19 pandemic and
+            help us bring their stores online
           </p>
           <Link to="/about">
             <button className={styles.ctaButton}>Learn More</button>
