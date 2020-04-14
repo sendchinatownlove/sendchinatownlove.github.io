8f8c490b74c6fbcb0473c7aae6198f93657f709e
stanleyzhengnyc <stanleyzhengnyc@users.noreply.github.com>
2020-04-14 02:01:04 +0000

    deploy: 8bc4421253591d905c58b056f8e9a4872d2a2ecc

---
+++ precache-manifest.472a445cfc2f7ace752ea7582aa1d406.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc17b192f58e041f78a8c5d9a1809638",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0f9f4d75cf9e823fbd2",
+    "url": "/static/css/main.78b323c5.chunk.css"
+  },
+  {
+    "revision": "2481051f0f6c02264c77",
+    "url": "/static/js/2.078e524e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.078e524e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0f9f4d75cf9e823fbd2",
+    "url": "/static/js/main.1b13b4c9.chunk.js"
+  },
+  {
+    "revision": "b991e9c21dba036bc800",
+    "url": "/static/js/runtime-main.f7beece5.js"
+  },
+  {
+    "revision": "909f1479b519e732b5a828cc5a69317a",
+    "url": "/static/media/logo.909f1479.svg"
+  }
+]);
