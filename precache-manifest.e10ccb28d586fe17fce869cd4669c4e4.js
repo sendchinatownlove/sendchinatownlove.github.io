96dc96bf86f1b59bc38e0440240f009cd60b170a
Justin McKibben <justintmckibben@gmail.com>
2020-04-13 23:32:15 -0500

    Updates

---
+++ precache-manifest.e10ccb28d586fe17fce869cd4669c4e4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2770e6cd3c0dce62b157535914d88414",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c0e3543270783e96c789",
+    "url": "/static/css/main.41d926b7.chunk.css"
+  },
+  {
+    "revision": "c7e269e5219a2281ba49",
+    "url": "/static/js/2.9a155418.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9a155418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0e3543270783e96c789",
+    "url": "/static/js/main.bedef09d.chunk.js"
+  },
+  {
+    "revision": "7ae0be752f21e964cf96",
+    "url": "/static/js/runtime-main.242ae121.js"
+  },
+  {
+    "revision": "349de0ef47f1b994db493c4d3776c2d4",
+    "url": "/static/media/chinatown-logo.349de0ef.png"
+  },
+  {
+    "revision": "4a2be72851ef3a703d4ed8cd4f1c3bd1",
+    "url": "/static/media/chinatown-splash.4a2be728.png"
+  },
+  {
+    "revision": "67dd739116e104ca62265f818c797ad2",
+    "url": "/static/media/chinatown-splash.67dd7391.png"
+  },
+  {
+    "revision": "909f1479b519e732b5a828cc5a69317a",
+    "url": "/static/media/logo.909f1479.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.4f08421eef17fc1138aaec0e96e2f4cb.js"
+  "/precache-manifest.e10ccb28d586fe17fce869cd4669c4e4.js"
 );
 
 self.addEventListener('message', (event) => {
