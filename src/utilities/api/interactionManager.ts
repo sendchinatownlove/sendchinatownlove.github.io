ecc0b8359138b7b6340fb5d21881feb666127172
abraham villaroman <bvillaroman@gmail.com>
2020-04-18 17:17:37 -0400

    Revert "removed interaction with stripe and added in comments for next steps"
    This reverts commit f217bd56928bc436972d136ba21b846288949780.

--- src/utilities/api/interactionManager.ts
+++ src/utilities/api/interactionManager.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
-// import { CardElement } from '@stripe/react-stripe-js';
-// import { Buyer, PaymentParams } from './types';
+import { CardElement } from '@stripe/react-stripe-js';
+import { Buyer, PaymentParams } from './types';
 import { charges, sellers } from './endpoints';
 
 // Fix return typing
@@ -17,19 +17,55 @@ export const getSeller = async (id: string) =>
     .then((res) => res)
     .catch((err) => err);
 
-// TODO(Bruce): add new api call for square 
-export const makePayment = async () => {
+// TODO(ArtyEmsee): add typing for stripe elements
+export const makePayment = async (
+  stripe: any,
+  elements: any,
+  payment: PaymentParams,
+  buyer: Buyer
+) => {
+  const { email, name } = buyer;
 
+  // TODO(ArtyEmsee): abstract api call, create global object for headers
   await axios
     .post(
       charges,
-      {},
+      {
+        line_items: [payment],
+        email: email,
+      },
       { headers: { 'Access-Control-Allow-Origin': '*' } }
     )
     .then(async (res) => {
-      console.log(res)
-      return res
-    })
-    .catch(err => err);
-};
+      // TODO(ArtyEmsee): fix response to success
+      if (!stripe || !elements) return;
+      else {
+        const cardElement = elements!.getElement(CardElement);
+        const result = await stripe!.confirmCardPayment(
+          `${res.data.client_secret}`,
+          {
+            payment_method: {
+              card: cardElement!,
+              billing_details: {
+                name: name,
+                email: email
+              },
+            },
+          }
+        );
 
+        if (result.error) {
+          console.log(result.error.message);
+        } else {
+          if (result.paymentIntent?.status === 'succeeded') {
+            console.log(
+              result.paymentIntent?.status,
+              'The payment has been processed!'
+            );
+          }
+        }
+      }
+    });
+
+  // TODO(ArtyEmsee): fix response to error
+};
