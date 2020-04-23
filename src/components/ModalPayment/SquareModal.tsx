b92ea316918778eff281bf95cd01961c2daa9e1b
John Tieu <johnson.tieu@gmail.com>
2020-04-23 16:12:56 -0400

    css naming

--- src/components/ModalPayment/SquareModal.tsx
+++ src/components/ModalPayment/SquareModal.tsx
@@ -67,11 +67,11 @@ const ModalPayment = ({ purchaseType, sellerId, sellerName }: Props) => {
           const newErrors =
             errorMessages.length > 0
               ? [
-                  ...errorMessages,
-                  responseErrors.map(
-                    (error: { detail: string }) => error.detail
-                  ),
-                ]
+                ...errorMessages,
+                responseErrors.map(
+                  (error: { detail: string }) => error.detail
+                ),
+              ]
               : responseErrors.map((error: { detail: string }) => error.detail);
           setErrorsMessages(newErrors);
         }
@@ -140,7 +140,7 @@ const ModalPayment = ({ purchaseType, sellerId, sellerName }: Props) => {
           </span>
           <p />
           <div>
-            <label className={styles.tc}>
+            <label className={styles.termsAndConditions}>
               <input
                 type="checkbox"
                 id="checkbox"
@@ -161,14 +161,14 @@ const ModalPayment = ({ purchaseType, sellerId, sellerName }: Props) => {
               exchanged for this donation.
             </p>
           ) : (
-            <p>
-              By proceeding with your purchase, you understand that the gift
+              <p>
+                By proceeding with your purchase, you understand that the gift
               card is not redeemable for cash and can only be used at{' '}
-              {sellerName}. All purchases are final. In the event that the
+                {sellerName}. All purchases are final. In the event that the
               merchant is no longer open at the time of redemption, Send
               Chinatown Love Inc. will not be able to refund your purchase.
-            </p>
-          )}
+              </p>
+            )}
           <div className={styles.btnRow}>
             <button
               type="button"
