de8789b3a339614b657e91460981994db5290693
abraham villaroman <bvillaroman@gmail.com>
2020-04-27 23:49:22 -0400

    added basic structure for 404 page

---
+++ src/components/404Page/ErrorPage.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {
+  
+}
+
+const ErrorPage = (props: Props) => {
+  return (
+    <div>
+      404 page
+    </div>
+  )
+}
+
+export default ErrorPage
