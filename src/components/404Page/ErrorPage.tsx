2ff3823021680b0bcd37a861039f781b6a8f97a1
abraham villaroman <bvillaroman@gmail.com>
2020-05-01 10:50:11 -0400

    added 404 image and made design expand whole screen/centered

--- src/components/404Page/ErrorPage.tsx
+++ src/components/404Page/ErrorPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import NavBar from '../Navbar';
 import styles from './styles.module.scss';
 import { Link } from 'react-router-dom';
+import ErrorImage from '../../images/404-error-image.png';
 
 interface Props {}
 
@@ -10,6 +11,7 @@ const ErrorPage = (props: Props) => {
     <main className={styles.container}>
       <NavBar />
       <div className={styles.textContainer}>
+        <img src={ErrorImage} alt="error" />
         <h1 className={styles.mainHeader}>Dumpling Not Found!</h1>
         <h3 className={styles.subHeader}>
           The page you are trying to access does not exist or has been moved.
@@ -24,3 +26,24 @@ const ErrorPage = (props: Props) => {
 };
 
 export default ErrorPage;
+// curl --location --request POST 'http://localhost:5000' \
+// --header 'Content-Type: application/json' \
+// --data-raw '{
+//     "seller_id": "shunfa-bakery",
+//     "cuisine_name": "Bakery",
+//     "name": "Shunfa Bakery",
+//     "story": "When Shunfa Bakery, a ",
+//     "accept_donations": true,
+//     "sell_gift_cards": true,
+//     "owner_name": "Shunfa Bakery",
+//     "owner_image_url": "assets/shunfa-bakery-logo.png",
+//     "target_amount": 1000000,
+//     "summary": null,
+//     "hero_image_url": "assets/shunfa-bakery-hero.png",
+//     "progress_bar_color": null,
+//     "business_type": "Family-owned",
+//     "num_employees": 3,
+//     "founded_year": 2015,
+//     "website_url": null,
+//     "menu_url": null
+// }'
