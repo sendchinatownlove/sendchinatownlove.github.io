56aca0426c182adfb0f47b214c5915c0041af39b
abraham villaroman <bvillaroman@gmail.com>
2020-05-06 09:39:39 -0400

    rebasing

---
+++ src/__tests__/SellerPage.test.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { render, fireEvent, waitForElement } from "@testing-library/react";
+
+import SellerPage from "../components/SellerPage";
+
+describe("<LoginForm />", () => {
+  test("should display a blank login form, with remember me checked by default", async () => {
+    
+    // ???
+  });
+});
