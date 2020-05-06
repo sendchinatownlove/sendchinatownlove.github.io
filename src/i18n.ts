dca6013f75416a3e78f7d29d9434f9d6a5ed4b46
abraham villaroman <bvillaroman@gmail.com>
2020-05-06 18:33:10 -0400

    added code splitting/loaders/styled components

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,17 @@
-import i18n from "i18next";
-import { initReactI18next } from "react-i18next";
+import i18n from 'i18next';
+import { initReactI18next } from 'react-i18next';
 
 // the translations
-import EnTrans from "./locales/en/translation.json"
-import CnTrans from "./locales/cn/translation.json"
-
+import EnTrans from './locales/en/translation.json';
+import CnTrans from './locales/cn/translation.json';
 
 i18n
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     initImmediate: false,
-    preload: ["en", "cn"],
-    fallbackLng: "en",
-    lng: "en",
+    preload: ['en', 'cn'],
+    fallbackLng: 'en',
+    lng: 'en',
 
     // have a common namespace used around the full app
     ns: ['translation'],
@@ -26,12 +25,12 @@ i18n
 
     resources: {
       en: {
-        translation: EnTrans
+        translation: EnTrans,
       },
       jap: {
-        translation: CnTrans
-      }
-    }
+        translation: CnTrans,
+      },
+    },
   });
 
-  export default i18n;
+export default i18n;
