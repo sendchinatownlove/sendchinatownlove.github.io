d3a98d26a007914cebaa5774e0bced517f2acd2e
abraham villaroman <bvillaroman@gmail.com>
2020-05-07 10:46:17 -0400

    - fixed hiding 404 page display when going to mobile - added styled-components 404 page/NavBar removed .scss files

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import styles from './styles.module.scss';
+import styled from "styled-components"
 import { Link } from 'react-router-dom';
 import MenuIcon from '@material-ui/icons/Menu';
 import CloseIcon from '@material-ui/icons/Close';
@@ -8,6 +8,9 @@ import sclLogo from '../../images/logo-with-text.png';
 interface Props {
   menuOpen: boolean;
   setMenuOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+interface CompactProps {
+  compact: boolean;
 }
 
 const NavBar = (props: Props) => {
@@ -18,6 +21,7 @@ const NavBar = (props: Props) => {
       setHamburgerOpen(true);
     } else {
       setHamburgerOpen(false);
+      props.setMenuOpen(false);
     }
   };
 
@@ -38,50 +42,88 @@ const NavBar = (props: Props) => {
     return !props.menuOpen ? (
       <MenuIcon onClick={(e) => props.setMenuOpen(true)} />
     ) : (
-      <div className={styles.navLinksCompactContainer}>
-        <div className={styles.navBarContainer}>
-          <img className={styles.image} src={sclLogo} alt="Logo" />
-          <CloseIcon
-            onClick={(e) => props.setMenuOpen(false)}
-            className={styles.closeIcon}
-          />
-        </div>
-        <a
-          href="https://sendchinatownlove.com/"
-          className={styles.navCompactLink}
-        >
-          HOME
-        </a>
-        <Link to="/sellers" className={styles.navCompactLink}>
-          MERCHANTS
-        </Link>
-        <Link to="/about" className={styles.navCompactLink}>
-          ABOUT US
-        </Link>
-      </div>
+      <NavLinksContainer compact={hamburgerOpen}>
+        <NavBarContainer>
+          <Image src={sclLogo} alt="Logo" />
+          <Close onClick={(e) => props.setMenuOpen(false)}/>
+        </NavBarContainer>
+        <NavLink compact={hamburgerOpen} href="https://sendchinatownlove.com/" >HOME</NavLink>
+        <ReactNavLink to="/sellers" compact={hamburgerOpen}>MERCHANTS</ReactNavLink>
+        <ReactNavLink to="/about" compact={hamburgerOpen}>ABOUT US</ReactNavLink>
+      </NavLinksContainer>
     );
   };
 
   return (
-    <div className={styles.navBarContainer}>
-      <img className={styles.image} src={sclLogo} alt="Logo" />
+    <NavBarContainer>
+      <Image src={sclLogo} alt="Logo" />
       {hamburgerOpen ? (
         showCompactMenu()
       ) : (
-        <div className={styles.navLinksContainer}>
-          <a href="https://sendchinatownlove.com/" className={styles.navLink}>
-            HOME
-          </a>
-          <Link to="/sellers" className={styles.navLink}>
-            MERCHANTS
-          </Link>
-          <Link to="/about" className={styles.navLink}>
-            ABOUT US
-          </Link>
-        </div>
+        <NavLinksContainer compact={hamburgerOpen}>
+          <NavLink compact={hamburgerOpen} href="https://sendchinatownlove.com/">HOME</NavLink>
+          <ReactNavLink to="/sellers" compact={hamburgerOpen}>MERCHANTS</ReactNavLink>
+          <ReactNavLink to="/about" compact={hamburgerOpen}>ABOUT US</ReactNavLink>
+        </NavLinksContainer>
       )}
-    </div>
+    </NavBarContainer>
   );
 };
 
 export default NavBar;
+
+const NavBarContainer = styled.div`
+  background-color: transparent;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  width: 90%;
+  margin: 15px auto;
+  align-items: center;
+  font-family: futura;
+`
+const Image = styled.img`
+  width: 150px;
+  height: 66px;
+`
+
+const NavLinksContainer = styled.div`
+  display: flex;
+  flex-direction: ${ (props: CompactProps) => props.compact ? `column` : 'row'};
+  width: ${(props: CompactProps) => props.compact ? `100%` : '50%'};
+  ${(props: CompactProps) => props.compact ? `
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right:0;
+    background-color: white;
+    // height: 100%;
+    z-index: 10;
+  ` :  `
+    max-width: 400px;
+    justify-content: space-between;
+  `
+  }
+`
+const NavLink = styled.a`
+  text-decoration: none;
+  color: black;
+  ${(props: CompactProps) => props.compact && `
+    width: 100%;
+    margin: 16px auto;
+    text-align: center;
+  `}
+`
+const ReactNavLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  ${(props: CompactProps) => props.compact && `
+    width: 100%;
+    margin: 16px auto;
+    text-align: center;
+  `}  
+`
+const Close = styled(CloseIcon)`
+  cursor: pointer;
+`
