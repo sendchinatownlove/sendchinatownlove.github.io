355ea18f586507d9961246591f0261f0b887cd61
Dan Deng <danthaman@fb.com>
2020-05-08 00:20:54 -0400

    Added navigation header to merchant page

--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import styles from './styles.module.scss';
 import sclLogo from './scl-logo.png';
+import NavBar from '../Navbar';
+import { useState } from 'react';
+import styled from 'styled-components';
 
 type Props = {
   name: string;
 };
 const HeroBanner: React.SFC<Props> = ({ name }) => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <div className={styles.container}>
-      <img className={styles.image} src={sclLogo} alt="Logo" />
+      <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className={styles.header}>{name}</div>
     </div>
   );
