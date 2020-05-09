7cd3c88c8b9bb2685aab71a1cd4ad4e077a44ed9
Stanley Zheng <stanley@zheng.nyc>
2020-05-08 22:17:02 -0400

    uupdate og tag snapshotting

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { hydrate, render } from "react-dom";
 import './index.scss';
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+
+// render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
+const rootElement = document.getElementById("root");
+
+if (rootElement) {
+  if (rootElement.hasChildNodes()) {
+    hydrate(<React.StrictMode>
+      <App />
+    </React.StrictMode>, rootElement);
+  } else {
+    render(<React.StrictMode>
+      <App />
+    </React.StrictMode>, rootElement);
+  }
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
