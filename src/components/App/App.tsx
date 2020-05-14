91f6b6459c80ad18e03e486caee3f9d54e04d299
abraham villaroman <bvillaroman@gmail.com>
2020-05-14 02:02:19 -0400

    removed header and footer from voucher

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -41,9 +41,6 @@ const App = () => {
       case 'seller':
         component = <SellerPage menuOpen={menuOpen} />;
         break;
-      case 'voucher':
-        component = <VoucherRedemptionPage menuOpen={menuOpen} />;
-        break;
       default:
         component = <ErrorPage menuOpen={menuOpen} />;
         break;
@@ -62,7 +59,9 @@ const App = () => {
     <Router history={history}>
       <Suspense fallback={<Loader isPage={true} />}>
         <Switch>
-          <Route path="/voucher">{returnComponent('voucher')}</Route>
+          <Route path="/voucher">
+            <VoucherRedemptionPage/>
+          </Route>
           <Route path="/merchants">{returnComponent('merchants')}</Route>
           <Route path="/:id">{returnComponent('seller')}</Route>
           <Route path="/:id#story">{returnComponent('seller')}</Route> */}
