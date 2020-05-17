7cea60df0e80f6267325d884d84a44d96cb246ee
abraham villaroman <bvillaroman@gmail.com>
2020-05-17 11:54:07 -0400

    added locales for confirmation modal

--- src/components/ModalConfirmation/ModalConfirmation.tsx
+++ src/components/ModalConfirmation/ModalConfirmation.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import classnames from 'classnames';
 import styles from './styles.module.scss';
 import confirmationPic from './chinatown-logo.png';
+import { useTranslation } from 'react-i18next';
 
 import { useModalPaymentDispatch } from '../../utilities/hooks/ModalPaymentContext/context';
 import { CLOSE_MODAL } from '../../utilities/hooks/ModalPaymentContext/constants';
@@ -14,15 +15,20 @@ export type Props = {
 
 const ModalConfirmation = (props: Props) => {
   const dispatch = useModalPaymentDispatch();
+  const { t } = useTranslation();
 
   return (
     <div className={styles.container} data-testid="Modal Confirmation">
-      <h2>Thank you!</h2>
+      <h2>{t('paymentProcessing.confirmation.header')}</h2>
       <p>
-        We appreciate your support. We'll{' '}
+        {t('paymentProcessing.confirmation.subHeader1')}
         {props.purchaseType === 'gift_card'
-          ? `email you your voucher when ${props.sellerName} opens back up!`
-          : `let you know when ${props.sellerName} receives your donation!`}
+          ? `${t('paymentProcessing.confirmation.voucherText1')}${
+              props.sellerName
+            }${t('paymentProcessing.confirmation.voucherText2')}`
+          : `${t('paymentProcessing.confirmation.donationText1')}${
+              props.sellerName
+            }${t('paymentProcessing.confirmation.donationText2')}`}
       </p>
 
       <img className={styles.image} src={confirmationPic} alt="Logo" />
