a0636b4d0b2dd3e61e3602ea059f255dc5a67d98
abraham villaroman <bvillaroman@gmail.com>
2020-05-17 23:43:27 -0400

    adding error handling for voucher page

--- src/pages/VoucherRedemption/VoucherRedemption.tsx
+++ src/pages/VoucherRedemption/VoucherRedemption.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
-import { useLocation } from 'react-router-dom';
+import { useHistory } from 'react-router-dom';
 
 import Landing from './Landing';
 import Amount from './Amount';
@@ -12,34 +12,43 @@ import {
   useVoucherDispatch,
 } from '../../utilities/hooks/VoucherContext/context';
 import { SET_VOUCHER_INFO } from '../../utilities/hooks/VoucherContext/constants';
+import Loader from '../../components/Loader';
 
 import { getVoucher, getSeller } from '../../utilities/api/interactionManager';
 
 interface Props {}
 
 const VoucherRedemption = (props: Props) => {
-  const { view } = useVoucherState();
+  const { view, voucher } = useVoucherState();
   const dispatch = useVoucherDispatch();
-  const params = useLocation();
+  const params = useHistory();
+  const [loading, setLoading] = useState(false);
 
   const fetchData = async () => {
-    const {
-      data: { gift_card_detail, seller_id },
-    } = await getVoucher(params.pathname.replace('/voucher/', ''));
-    const merchantData = await getSeller(seller_id);
+    try {
+      const {
+        data: { gift_card_detail, seller_id },
+      } = await getVoucher(params.location.pathname.replace('/voucher/', ''));
+      const merchantData = await getSeller(seller_id);
 
-    const voucher = {
-      ...gift_card_detail,
-      ownerName: merchantData.data.owner_name,
-      ownerImage: merchantData.data.owner_image_url,
-      sellerID: seller_id,
-      locations: merchantData.data.locations,
-    };
+      const voucher = {
+        ...gift_card_detail,
+        ownerName: merchantData.data.owner_name,
+        ownerImage: merchantData.data.owner_image_url,
+        sellerID: seller_id,
+        locations: merchantData.data.locations,
+      };
 
-    dispatch({ type: SET_VOUCHER_INFO, payload: voucher });
+      dispatch({ type: SET_VOUCHER_INFO, payload: voucher });
+      setLoading(false);
+    } catch {
+      params.push('/');
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
+    setLoading(true);
     fetchData();
   }, []);
 
@@ -57,8 +66,14 @@ const VoucherRedemption = (props: Props) => {
   };
   return (
     <Container>
-      <StoreBanner />
-      {showView()}
+      {loading && voucher.sellerID.length > 1 ? (
+        <Loader isPage={true} />
+      ) : (
+        <>
+          <StoreBanner />
+          {showView()}
+        </>
+      )}
     </Container>
   );
 };
