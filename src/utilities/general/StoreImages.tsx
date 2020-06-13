5b00f79d19c26ba0b41d3702a24f22095ae4643b
Athena Chen <athenachenn@gmail.com>
2020-06-13 18:10:31 -0400

    changed urls

--- src/utilities/general/StoreImages.tsx
+++ src/utilities/general/StoreImages.tsx
@@ -2,8 +2,8 @@
 export const getWebsiteImages = () => {
   return {
     merchantHero:
-      'https://storage.cloud.google.com/sendchinatownlove-assets/public/assets/general/merchant-directory-hero.png',
+      'https://storage.googleapis.com/sendchinatownlove-assets/public/assets/general/merchant-directory-hero.png',
     skyline:
-      'https://storage.cloud.google.com/sendchinatownlove-assets/public/assets/general/skyline-hero.png',
+      'https://storage.googleapis.com/sendchinatownlove-assets/public/assets/general/skyline-hero.png',
   };
 };
