941fc8ea2469c7e06dc6fabda48c62ac8f3561cc
justintmckibben <justintmckibben@users.noreply.github.com>
2020-06-23 00:17:00 +0000

    deploy: a1069a1c7d1c7441435b4fe0ba89035389fd5203

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3fd9b1c5d2b64c096be6a1e38731de7b.js"
+  "/precache-manifest.991f02fc63f136ac0614bbc21e77afba.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -34,6 +34,6 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
-
+  
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
