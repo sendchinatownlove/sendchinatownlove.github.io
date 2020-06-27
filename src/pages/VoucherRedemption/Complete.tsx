c265b24a7229363ffcbcf00942143def271d8f10
abraham villaroman <bvillaroman@gmail.com>
2020-06-27 14:14:29 -0400

    finished complete page

--- src/pages/VoucherRedemption/Complete.tsx
+++ src/pages/VoucherRedemption/Complete.tsx
@@ -7,11 +7,17 @@ import {
   SET_AMOUNT,
   SET_VIEW,
 } from '../../utilities/hooks/VoucherContext/constants';
-import { AmountContainer, Text, Footer, NextButton } from './styles';
+import styled from 'styled-components';
+import {
+  ViewContainer,
+  SubViewContainer,
+  Text,
+  Footer,
+  SubmitButton,
+} from './style';
 import StoreBanner from './StoreBanner';
 
-interface Props {}
-const Amount = (props: Props) => {
+const Amount = () => {
   const { amount, voucher } = useVoucherState();
   const dispatch = useVoucherDispatch();
 
@@ -21,34 +27,34 @@ const Amount = (props: Props) => {
   };
 
   return (
-    <>
+    <ViewContainer>
       <StoreBanner />
-      <Footer height="90vh" style={{ flex: 1 }}>
-        <AmountContainer height="35px">
-          <Text bold="true" size="24px">
-            Redemption Complete
-          </Text>
-        </AmountContainer>
+      <MainView>
+        <BoldText>Redemption Complete</BoldText>
         {!voucher.single_use ? (
-          <AmountContainer height="55px">
-            <Text bold="true" size="24px">
-              $ {(amount / 100).toFixed(2)}
-            </Text>
-            <Text size="16px">Remaining voucher balance</Text>
-          </AmountContainer>
+          <SubViewContainer>
+            <Text>$ {(amount / 100).toFixed(2)}</Text>
+            <Text>Remaining voucher balance</Text>
+          </SubViewContainer>
         ) : (
           ''
         )}
-        <br />
-        <Text size="24px" bold="true" width="80%" align="center">
-          Thank you for dining at {voucher.storeName}!
-        </Text>
-        <br />
-        <br />
-        <NextButton onClick={(e) => setView(0)}>Finish</NextButton>
-      </Footer>
-    </>
+        <BoldText> Thank you for dining at {voucher.storeName}! </BoldText>
+        <SubmitButton onClick={(e) => setView(0)}>Finish</SubmitButton>
+      </MainView>
+    </ViewContainer>
   );
 };
 
 export default Amount;
+
+const MainView = styled(Footer)`
+  min-height: 90%;
+  justify-content: space-around;
+`;
+
+const BoldText = styled(Text)`
+  font-weight: 600;
+  font-size: 22px;
+  line-height: 31px;
+`;
