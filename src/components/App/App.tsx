b7c678b2080ecba2351c40039742a8aac330d83a
Justin McKibben <justintmckibben@gmail.com>
2020-07-11 18:48:52 -0500

    Add gift a meal gofundme route

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -84,6 +84,10 @@ const App = () => {
               <VoucherRedemptionPage />
             </VoucherProvider>
           </Route>
+          <Route path='/gift-a-meal' component={() => {
+              window.location.href = 'https://www.gofundme.com/f/gift-a-meal';
+              return null;
+          }}/>
           <Route path="/merchants">{returnComponent('merchants')}</Route>
           <Route path="/:id">{returnComponent('seller')}</Route>
           <Route path="/:id#story">{returnComponent('seller')}</Route>
