149ef74b420e552162748840c9e6ce8a9a5939b1
abraham villaroman <bvillaroman@gmail.com>
2020-07-19 19:25:42 -0400

    - updating voucher context to a custom react hook to be used in tests

---
+++ src/utilities/hooks/VoucherContext/index.ts
@@ -0,0 +1,18 @@
+import * as VoucherConstants from './constants';
+import VoucherStore from './context';
+import VoucherReducer from './reducers';
+import * as VoucherTypes from './types';
+
+const [
+  VoucherProvider,
+  useVoucherState, 
+  useVoucherDispatch
+] = VoucherStore(VoucherReducer, VoucherTypes.defaultState);
+
+export {
+  VoucherProvider,
+  useVoucherState, 
+  useVoucherDispatch, 
+  VoucherConstants, 
+  VoucherTypes
+};
