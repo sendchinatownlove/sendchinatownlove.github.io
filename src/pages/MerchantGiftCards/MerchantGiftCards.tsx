f3885bb96697f4d8fc11ee609aced2085fb84c62
Jacob Barnett <jacob@havenlife.com>
2020-07-26 01:37:35 -0400

    Adding bootstrap and lots of additional styling

--- src/pages/MerchantGiftCards/MerchantGiftCards.tsx
+++ src/pages/MerchantGiftCards/MerchantGiftCards.tsx
@@ -2,9 +2,8 @@ import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import { getMerchantGiftCards } from '../../utilities/api/interactionManager';
 import Loader from '../../components/Loader/Loader';
-import styles from './styles.module.scss'
-import { sellers } from '../../utilities/api/endpoints'
-import Pagination from 'react-bootstrap/Pagination'
+import styles from './styles.module.scss';
+import { sellers } from '../../utilities/api/endpoints';
 const FilterableTable = require('react-filterable-table');
 
 const MerchantGiftCards = () => {
@@ -16,13 +15,52 @@ const MerchantGiftCards = () => {
   const [loading, setLoading] = useState(false);
   const [giftCards, setGiftCards] = useState<any | null>();
 
+  const renderAmount = (props) => {
+    return Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+      maximumSignificantDigits: 4,
+    }).format(props.value / 100);
+  };
+
+  const renderDate = (props) => {
+    return new Date(props.value).toISOString().substring(0, 10);
+  };
+
   const fields = [
-    { name: 'seller_gift_card_id', displayName: 'Voucher ID', inputFilterable: true, sortable: true },
-    { name: 'value', displayName: 'Amount', inputFilterable: true, sortable: true },
-    { name: 'email', displayName: 'Email', inputFilterable: true, sortable: true },
-    { name: 'created_at', displayName: 'Date Created', inputFilterable: true, sortable: true },
-    { name: 'expiration', displayName: 'Expiration', inputFilterable: true, sortable: true }
-  ]
+    {
+      name: 'seller_gift_card_id',
+      displayName: 'Voucher ID',
+      inputFilterable: true,
+      sortable: true,
+    },
+    {
+      name: 'email',
+      displayName: 'Email',
+      inputFilterable: true,
+      sortable: true,
+    },
+    {
+      name: 'value',
+      displayName: 'Amount',
+      inputFilterable: true,
+      sortable: true,
+      render: renderAmount,
+    },
+    {
+      name: 'created_at',
+      displayName: 'Date Created',
+      inputFilterable: true,
+      sortable: true,
+      render: renderDate,
+    },
+    {
+      name: 'expiration',
+      displayName: 'Expiration',
+      inputFilterable: true,
+      sortable: true,
+    },
+  ];
 
   const fetchData = async () => {
     try {
@@ -54,37 +92,48 @@ const MerchantGiftCards = () => {
       ) : error ? (
         <p>Error!</p>
       ) : (
-          <>
-            <div className={styles.header}>
-                <h1>Voucher Tracker</h1>
-                <h2>Melonpanna Tea and Shot</h2>
+        <>
+          <div className={styles.header}>
+            <h1>Voucher Tracker</h1>
+            <h2>Melonpanna Tea and Shot</h2>
+          </div>
+          <div className={styles.metadataHeader}>
+            <div className={styles.metadataBlock}>
+              <h1>Last Updated</h1>
+              <h2>{new Date().toISOString().substring(0, 10)}</h2>
             </div>
-            <div className={styles.metadataHeader}>
-                <div className={styles.metadataBlock}>
-                    <h1>Last Updated</h1>
-                    <h2>2020-07-09</h2>
-                </div>
-                <div className={styles.metadataBlock}>
-                    <h1>Active Vouchers</h1>
-                    <h2>2</h2>
-                </div>
-                <div className={styles.metadataBlock}>
-                    <h1>Total Balance</h1>
-                    <h2>$100</h2>
-                </div>
+            <div className={styles.metadataBlock}>
+              <h1>Active Vouchers</h1>
+              <h2>{giftCards && giftCards.length}</h2>
             </div>
-            <FilterableTable
-                namespace="Gift Cards"
-                initialSort="seller_gift_card_id"
-                // data={giftCards}
-                dataEndpoint={sellers + urlParams[1] + '/gift_cards/' + urlParams[2]}
-                fields={fields}
-                noRecordsMessage="There are no people to display"
-                noFilteredRecordsMessage="No people match your filters!"
-                topPagerVisible={false}
-                pageSize={20}
-                pageSizes={null}
-            />
+            <div className={styles.metadataBlock}>
+              <h1>Total Balance</h1>
+              <h2>
+                {giftCards &&
+                  renderAmount({
+                    value: giftCards.reduce(
+                      (acc, cur) => acc.value + cur.value
+                    ),
+                  })}
+              </h2>
+            </div>
+          </div>
+          <FilterableTable
+            namespace="Gift Cards"
+            initialSort="seller_gift_card_id"
+            // data={giftCards}
+            dataEndpoint={
+              sellers + urlParams[1] + '/gift_cards/' + urlParams[2]
+            }
+            fields={fields}
+            noRecordsMessage="There are no vouchers to display"
+            noFilteredRecordsMessage="No vouchers match your filters!"
+            topPagerVisible={false}
+            pageSize={20}
+            pageSizes={null}
+            recordCountName="Voucher"
+            recordCountNamePlural="Vouchers"
+          />
         </>
       )}
     </>
