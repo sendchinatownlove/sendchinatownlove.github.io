fed66753f14758fb4a22b3ce3e6f77bb1442cbb5
abraham villaroman <bvillaroman@gmail.com>
2020-07-28 09:49:46 -0400

    changing modal payment context to use a custom react hook

--- src/utilities/hooks/ModalPaymentContext/context.tsx
+++ src/utilities/hooks/ModalPaymentContext/context.tsx
@@ -6,50 +6,62 @@
  */
 
 import React from 'react';
-import { defaultState } from './types';
-import ModalPaymentReducer, { Action } from './reducers';
+import { Action } from './reducers';
 
-// CONTEXT
-const ModalPaymentStateContext = React.createContext(defaultState);
-const ModalPaymentDispatchContext = React.createContext(
-  (() => null) as React.Dispatch<Action>
-);
+type ProviderProps = { children: React.ReactNode };
 
 /**
- * Modal Payment Provider
+ * Modal Payment Store
  *
- * @param {node} children - return the react components that are the going to use the context
+ * Custom react hook that make Modal Payment State and Dispatch private within its own provider
+ *
+ * @param {function} reducer - reducer function that controls the change of state according to its dispatcher
+ * @param {object} intialState - the intial state of our ModalPayment context
  * @return {node} The context that holds all state props/methods
  *
  */
+export default function ModalPaymentStore(reducer, intialState) {
+  // CONTEXT
+  const ModalPaymentStateContext = React.createContext(intialState);
+  const ModalPaymentDispatchContext = React.createContext(
+    (() => null) as React.Dispatch<Action>
+  );
+  const [state, dispatch] = React.useReducer(reducer, intialState);
 
-const ModalPaymentProvider = (props: any) => {
-  const [state, dispatch] = React.useReducer(ModalPaymentReducer, defaultState);
-
-  return (
+  /**
+   * Modal Provider
+   *
+   * @param {node} children - return the react components that are the going to use the context
+   * @return {node} The context that holds all state props/methods
+   *
+   */
+  const ModalPaymentProvider = ({ children }: ProviderProps) => (
     <ModalPaymentStateContext.Provider value={state}>
       <ModalPaymentDispatchContext.Provider value={dispatch}>
-        {props.children}
+        {children}
       </ModalPaymentDispatchContext.Provider>
     </ModalPaymentStateContext.Provider>
   );
-};
 
-function useModalPaymentState() {
-  const context = React.useContext(ModalPaymentStateContext);
-  if (context === undefined) {
-    throw new Error('useModalPaymentState must be used within a CountProvider');
+  function useModalPaymentState() {
+    const context = React.useContext(ModalPaymentStateContext);
+    if (context === undefined) {
+      throw new Error(
+        'useModalPaymentState must be used within a ModalPaymentProvider'
+      );
+    }
+    return context;
   }
-  return context;
-}
-function useModalPaymentDispatch() {
-  const context = React.useContext(ModalPaymentDispatchContext);
-  if (context === undefined) {
-    throw new Error(
-      'useModalPaymentDispatch must be used within a CountProvider'
-    );
-  }
-  return context;
-}
 
-export { ModalPaymentProvider, useModalPaymentDispatch, useModalPaymentState };
+  function useModalPaymentDispatch() {
+    const context = React.useContext(ModalPaymentDispatchContext);
+    if (context === undefined) {
+      throw new Error(
+        'useModalPaymentDispatch must be used within a ModalPaymentProvider'
+      );
+    }
+    return context;
+  }
+
+  return [ModalPaymentProvider, useModalPaymentState, useModalPaymentDispatch];
+}
