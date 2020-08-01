1ef466e43fdd792e12341ab18625cb45102fd563
abraham villaroman <bvillaroman@gmail.com>
2020-08-01 11:10:09 -0400

    updating modal tests to use new render method and new provider

--- src/utilities/testing/render.tsx
+++ src/utilities/testing/render.tsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import { MemoryRouter } from 'react-router-dom';
+import { I18nextProvider } from 'react-i18next';
+import i18n from '../../i18n';
+import { VoucherProvider } from '../../utilities/hooks/VoucherContext';
+import { ModalPaymentProvider } from '../../utilities/hooks/ModalPaymentContext';
 
 const renderIntegration = (initialPage = '', component: React.ReactNode) => {
   return render(
@@ -9,4 +13,14 @@ const renderIntegration = (initialPage = '', component: React.ReactNode) => {
   );
 };
 
-export { renderIntegration };
+const renderUnit = (component: React.ReactNode) => {
+  return render(
+    <I18nextProvider i18n={i18n}>
+      <VoucherProvider>
+        <ModalPaymentProvider>{component}</ModalPaymentProvider>
+      </VoucherProvider>
+    </I18nextProvider>
+  );
+};
+
+export { renderIntegration, renderUnit };
