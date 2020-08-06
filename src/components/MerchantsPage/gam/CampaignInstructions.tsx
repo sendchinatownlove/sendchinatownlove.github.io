763f4195a25040267b4f7b16fe542970bacbc6cd
liv-szt <oliviasztanga@gmail.com>
2020-08-06 09:35:03 -0400

    added no campaigns box

--- src/components/MerchantsPage/gam/CampaignInstructions.tsx
+++ src/components/MerchantsPage/gam/CampaignInstructions.tsx
@@ -7,36 +7,33 @@ import gam_icon_step3 from '../images/gam_icon_step3.svg';
 import gam_icon_step4 from '../images/gam_icon_step4.svg';
 
 const GiftAMeal = () => {
-    return (
-        <div className={styles.sub}>
-          <div className={styles.instructions}>
-            {[
-              ['Donate to Gift-a-Meal', gam_icon_step1],
-              [
-                '100% of donations still go directly to our merchants',
-                gam_icon_step2,
-              ],
-              [
-                'Vouchers are donated to our community parnters',
-                gam_icon_step3,
-              ],
-              ['Partners distribute to individuals in need', gam_icon_step4],
-            ].map(([text, icon], idx) => generateStep(idx + 1, text, icon))}
-          </div>
-        </div>
-    )
-}
-
-const generateStep = (n, text, icon) => {
-return (
-    <div className={styles.step} key={n}>
-    <img src={icon} alt="icon" />
-    <div className={styles.stepText}>
-        <p>Step {n}</p>
-        <p>{text}</p>
+  return (
+    <div className={styles.sub}>
+      <div className={styles.instructions}>
+        {[
+          ['Donate to Gift-a-Meal', gam_icon_step1],
+          [
+            '100% of donations still go directly to our merchants',
+            gam_icon_step2,
+          ],
+          ['Vouchers are donated to our community parnters', gam_icon_step3],
+          ['Partners distribute to individuals in need', gam_icon_step4],
+        ].map(([text, icon], idx) => generateStep(idx + 1, text, icon))}
+      </div>
     </div>
-    </div>
-);
+  );
 };
 
-export default GiftAMeal
+export default GiftAMeal;
+
+const generateStep = (n, text, icon) => {
+  return (
+    <div className={styles.step} key={n}>
+      <img src={icon} alt="icon" />
+      <div className={styles.stepText}>
+        <p>Step {n}</p>
+        <p>{text}</p>
+      </div>
+    </div>
+  );
+};
--- src/components/MerchantsPage/gam/GiftAMealPage.tsx
+++ src/components/MerchantsPage/gam/GiftAMealPage.tsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './styles.module.scss';
-
+import classnames from 'classnames';
 import illustrated_flatlay_hero from '../images/illustrated_flatlay_hero.png';
-import CampaginInstructions from './CampaignInstructions'
+import CampaginInstructions from './CampaignInstructions';
+import CampaignListItem from './CampaignListItem';
 
 interface Props {
   menuOpen: boolean;
 }
 
 const GiftAMealPage = (props: Props) => {
-
+  const [activeCampaigns, setActiveCampaigns] = useState([]);
+  const [pastCampaigns, setPastCampaigns] = useState([]);
+  
   return (
     <div
       className={styles.container}
@@ -21,7 +24,7 @@ const GiftAMealPage = (props: Props) => {
           className={styles.hero}
           alt="meal overlay illustration"
         />
-        <div className={styles.main}>
+        <div className={styles.header_text}>
           <div>
             <h3 style={{ fontWeight: 'bolder' }}>Gift a Meal</h3>
             <p>
@@ -35,11 +38,32 @@ const GiftAMealPage = (props: Props) => {
       </div>
       <br />
       <button className={styles.backButton}>Back to merchants</button>
-      <h5 className={styles.campaignHeader}>Active Gift-a-Meal</h5>
-      
+      {
+        activeCampaigns.length
+          ? <>
+              <h5 className={styles.campaignHeader}>Active Gift-a-Meal</h5>
+              {activeCampaigns.map((campaign: any) => (
+                // TODO: pass campaign data to CampaignListItem
+                <CampaignListItem campaign={''} />
+              ))}
+            </>
+          : <div className={styles.noCampaign}>
+              <div className={styles.noCampaignText}>
+                <p>No active campaigns? You can still donate!</p>
+                <p>Meal donations here will automatically be applied to our next campaign.</p>
+              </div>
+              <button 
+                className={classnames("button--filled", styles.giftButton)}
+              >
+                Gift Meal
+              </button>
+            </div>
+      }
       <h5 className={styles.campaignHeader}>Past Gift-a-Meal</h5>
-
-      
+      {pastCampaigns.map((campaign: any) => (
+        // TODO: pass campaign data to CampaignListItem
+        <CampaignListItem campaign={''} />
+      ))}
     </div>
   );
 };
