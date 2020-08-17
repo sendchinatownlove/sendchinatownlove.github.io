559f4e6d635773407f86f68e1be7ecd9bac5dff3
oliviasztanga <oliviasztanga@users.noreply.github.com>
2020-08-17 21:13:55 +0000

    deploy: 8c1f31eee9ded0fc4c3ae21357fdebba8591d42b

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.06dbd62f10caf38008cd6e1753933fb3.js"
+  "/precache-manifest.0e7bc7f37b775f573621bd5efbb45e4a.js"
 );
 
 self.addEventListener('message', (event) => {
