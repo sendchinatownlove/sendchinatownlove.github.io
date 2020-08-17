dafb6fab0bd3d9f76e9e5c3fe229bf8ff0f566dd
abraham villaroman <bvillaroman@gmail.com>
2020-08-17 18:49:20 -0400

    adding basic styling for ticket rows

--- src/pages/PassportRedemption/Passport.tsx
+++ src/pages/PassportRedemption/Passport.tsx
@@ -7,15 +7,45 @@ interface Props {
   setCurrentScreenView: Function;
 };
 
+type stampProp = {
+  sellerId: string;
+};
+
 const initStamps = [1,2,3,4,5,6,7,9];
 const Passport = ({ setCurrentScreenView }: Props) => {
   const [stamps, setStamps] = useState(initStamps);
 
   const createRow = (info,index) => {
+    const dummyInfo = {
+      id: 1,
+      contact_id: 12,
+      ticket_id: 123,
+      participating_seller_id: "WOK WOK",
+      sponsor_seller_id: "WOK WOK",
+      redeemed_at: "9/1/20",
+      expiration: "9/20/20"
+    }
+    
+
     return (
       <TableRow key={index}>
         <TableIndex> {index + 1} </TableIndex>
-        <TableStamp> {info} </TableStamp>
+        <TableStamp> 
+          <StampRow>
+            <Stamp sellerId={dummyInfo.participating_seller_id}>
+              {dummyInfo.participating_seller_id}
+            </Stamp>
+            <Stamp sellerId="CONGEE VILLAGE">
+              CONGEE VILLAGE
+            </Stamp>
+            <Stamp sellerId="46 MOTT">
+              46 MOTT
+            </Stamp>
+          </StampRow>
+          <RedeemedRow>
+            2 MORE STAMPS UNTIL YOUR NEXT REWARD
+          </RedeemedRow>
+        </TableStamp>
       </TableRow>
     ) 
   }
@@ -76,3 +106,38 @@ const AddNewTicket = styled(Button)`
   position: fixed;
   bottom: 10px;
 `
+const StampRow = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  height: 70px;
+`
+const RedeemedRow = styled.div`
+  border-top: 1px dotted #A5A5A5;
+  width: 100%;
+  margin: 0 auto;
+  text-align: center;
+  font-size: 10px;
+  height: 15px;
+`
+const Stamp = styled.div`
+  font-size: 10px;
+  height: 35px;
+  width: 60px;
+  padding: 5px;
+  ${(props: stampProp) => props.sellerId.length < 12 && `
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;    
+  `}
+  font-weight: bold;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  max-width: 75px;
+  text-align: center;
+  border: 2px solid #A8192E;
+  color: #A8192E;
+`;
