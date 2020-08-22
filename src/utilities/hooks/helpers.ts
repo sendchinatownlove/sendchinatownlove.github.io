0e07b461d0aabfa233e4eeefabaf75ad9a4ba021
Dan Deng <danthaman@fb.com>
2020-08-22 15:07:07 -0400

    Make entire highlight box clickable on mobile screen sizes

--- src/utilities/hooks/helpers.ts
+++ src/utilities/hooks/helpers.ts
@@ -1,4 +1,5 @@
 import { LocationInfo } from '../../utilities/hooks/VoucherContext/types';
+import { useState, useEffect } from 'react';
 
 export const getLocationInfo = (merchantData): LocationInfo => {
   const emptyLocationInfo = {
@@ -20,4 +21,28 @@ export const getLocationInfo = (merchantData): LocationInfo => {
     }`,
   };
   return locationInfo;
+};
+
+ // https://usehooks.com/useWindowSize/
+ export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowSize;
 };
