500147b5c1aff802098c6b5565aced393fc30bbd
Jacob Barnett <jacob@havenlife.com>
2020-09-21 18:53:46 -0400

    Adding react-markdown, implementing, minor styling

--- src/components/StoreStory/StoreStory.tsx
+++ src/components/StoreStory/StoreStory.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import styles from './styles.module.scss';
+import ReactMarkdown from "react-markdown";
 
 type Props = {
   story: string;
 };
 
 const StoreStory: React.SFC<Props> = ({ story }) => (
-  <section className={styles.container}>{story}</section>
+  <section className={styles.container}>
+    <ReactMarkdown source={story}/>
+  </section>
 );
 
 export default StoreStory;
