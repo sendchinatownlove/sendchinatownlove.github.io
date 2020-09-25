98fcccbca1c59a1d0a60c5c41e6964b189a3d445
Wilson <wilsonj806@gmail.com>
2020-09-25 12:19:47 -0400

    Run prettier on the GiveawayPopover

--- src/pages/PassportRedemption/GiveawayPopover.tsx
+++ src/pages/PassportRedemption/GiveawayPopover.tsx
@@ -1,97 +1,108 @@
-import React , { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-import GreenCheck from './GreenCheck'
-import InstagramEnabled from './InstagramEnabled'
+import GreenCheck from './GreenCheck';
+import InstagramEnabled from './InstagramEnabled';
 import InstagramDisabled from './InstagramDisabled';
 
-import { getAllParticipatingSellers, getGiveawayTicketsForContact } from '../../utilities/api/interactionManager'
-
+import {
+  getAllParticipatingSellers,
+  getGiveawayTicketsForContact,
+} from '../../utilities/api/interactionManager';
 
 interface Props {
-  showInstagram : boolean
-  contactId: string
+  showInstagram: boolean;
+  contactId: string;
 }
 
-
 interface GiveawayData {
-  weekly_giveaway_entries: String
-  number_of_visits_left : undefined | number
+  weekly_giveaway_entries: String;
+  number_of_visits_left: undefined | number;
 }
 
 const GiveawayPopover = (props: Props) => {
   const [showInfo, setShowInfo] = useState(true);
   const [giveawayData, setData] = useState<GiveawayData>({
     weekly_giveaway_entries: '',
-    number_of_visits_left: undefined
-  })
+    number_of_visits_left: undefined,
+  });
 
   async function setGiveawayCount(contactId: string) {
-      const { data: allSellers } = await getAllParticipatingSellers();
-      const { data: { weekly_giveaway_entries, unique_seller_tickets } } = await getGiveawayTicketsForContact(contactId)
-      setData({weekly_giveaway_entries, number_of_visits_left: allSellers.length - unique_seller_tickets})
+    const { data: allSellers } = await getAllParticipatingSellers();
+    const {
+      data: { weekly_giveaway_entries, unique_seller_tickets },
+    } = await getGiveawayTicketsForContact(contactId);
+    setData({
+      weekly_giveaway_entries,
+      number_of_visits_left: allSellers.length - unique_seller_tickets,
+    });
   }
   useEffect(() => {
     if (props.showInstagram && showInfo) {
-      setGiveawayCount(props.contactId)
+      setGiveawayCount(props.contactId);
     }
-  }, [props.showInstagram, showInfo, props.contactId])
+  }, [props.showInstagram, showInfo, props.contactId]);
   return (
     <>
-      {
-        props.showInstagram ?
+      {props.showInstagram ? (
         <InstagramButtonEnabled onClick={() => setShowInfo(!showInfo)}>
-          <InstagramEnabled/>
-        </InstagramButtonEnabled> :
+          <InstagramEnabled />
+        </InstagramButtonEnabled>
+      ) : (
         <InstagramButtonDisabled>
-          <InstagramDisabled/>
+          <InstagramDisabled />
         </InstagramButtonDisabled>
-      }
-    <PopoverContainer>
-      {props.showInstagram && showInfo && <PopoverContent>
-        <ToggleClosedButton onClick={() => setShowInfo(false)}>
-          <InstagramEnabled/>
-        </ToggleClosedButton>
-        <Title>
-          INSTAGRAM FOR GIVEAWAY ADDED
-        </Title>
-        <Divider/>
-        <GiveawayTextContainer>
-          <InfoSentence>
-            <DynamicBoldText>{giveawayData.weekly_giveaway_entries}</DynamicBoldText>
-            <GeneralText>
-              Weekly Giveaway Entries
-            </GeneralText>
-          </InfoSentence>
+      )}
+      <PopoverContainer>
+        {props.showInstagram && showInfo && (
+          <PopoverContent>
+            <ToggleClosedButton onClick={() => setShowInfo(false)}>
+              <InstagramEnabled />
+            </ToggleClosedButton>
+            <Title>INSTAGRAM FOR GIVEAWAY ADDED</Title>
+            <Divider />
+            <GiveawayTextContainer>
+              <InfoSentence>
+                <DynamicBoldText>
+                  {giveawayData.weekly_giveaway_entries}
+                </DynamicBoldText>
+                <GeneralText>Weekly Giveaway Entries</GeneralText>
+              </InfoSentence>
 
-          {giveawayData.number_of_visits_left !== 0 ?
-          <InfoSentence>
-            <DynamicBoldText>{giveawayData.number_of_visits_left}</DynamicBoldText>
-            <GeneralText>
-              Vendor Tickets away from a Grand Prize Giveaway Entry
-            </GeneralText>
-          </InfoSentence> :
-          <InfoSentence>
-            <IconWrap>
-              <GreenCheck/>
-            </IconWrap>
-            <GeneralText>Grand Prize Giveaway Entered</GeneralText>
-          </InfoSentence>
-          }
-        </GiveawayTextContainer>
-        <SubText>
-        Weekly Giveaway winners are announced on Instagram each Monday during the month of September. Grand Prize winners will be announced on October 1st.
-        </SubText>
-        <CloseButton type='button' onClick={() => setShowInfo(false)}>
-          CLOSE
-        </CloseButton>
-      </PopoverContent>}
-    </PopoverContainer>
+              {giveawayData.number_of_visits_left !== 0 ? (
+                <InfoSentence>
+                  <DynamicBoldText>
+                    {giveawayData.number_of_visits_left}
+                  </DynamicBoldText>
+                  <GeneralText>
+                    Vendor Tickets away from a Grand Prize Giveaway Entry
+                  </GeneralText>
+                </InfoSentence>
+              ) : (
+                <InfoSentence>
+                  <IconWrap>
+                    <GreenCheck />
+                  </IconWrap>
+                  <GeneralText>Grand Prize Giveaway Entered</GeneralText>
+                </InfoSentence>
+              )}
+            </GiveawayTextContainer>
+            <SubText>
+              Weekly Giveaway winners are announced on Instagram each Monday
+              during the month of September. Grand Prize winners will be
+              announced on October 1st.
+            </SubText>
+            <CloseButton type="button" onClick={() => setShowInfo(false)}>
+              CLOSE
+            </CloseButton>
+          </PopoverContent>
+        )}
+      </PopoverContainer>
     </>
-  )
-}
+  );
+};
 
-export default GiveawayPopover
+export default GiveawayPopover;
 
 const PopoverContainer = styled.div`
   position: absolute;
@@ -100,21 +111,21 @@ const PopoverContainer = styled.div`
 `;
 
 const InstagramButtonBase = styled.div`
-position: absolute;
-text-decoration: none;
-color: black;
-padding: 10px 15px;
-font-size: 22px;
-`
+  position: absolute;
+  text-decoration: none;
+  color: black;
+  padding: 10px 15px;
+  font-size: 22px;
+`;
 
 const InstagramButtonDisabled = styled(InstagramButtonBase)`
   right: 9px;
   top: 7px;
-`
+`;
 const InstagramButtonEnabled = styled(InstagramButtonBase)`
   right: 4px;
   top: 11px;
-`
+`;
 
 const PopoverContent = styled.div`
   position: absolute;
@@ -126,12 +137,12 @@ const PopoverContent = styled.div`
   height: 315px;
   right: 12.5px;
   top: -1px;
-  background: #F2EAE8;
+  background: #f2eae8;
   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(4px);
   border-radius: 13px;
   z-index: 2;
-`
+`;
 
 const Title = styled.h1`
   position: absolute;
@@ -143,24 +154,24 @@ const Title = styled.h1`
   display: flex;
   align-items: center;
   letter-spacing: 0.15em;
-`
+`;
 const Divider = styled.div`
   position: absolute;
   width: 237px;
   top: 52px;
   margin: 0;
   border-bottom: 1px solid #8e8e8e;
-`
+`;
 
 const GiveawayTextContainer = styled.div`
   width: 258px;
   position: absolute;
   top: 56px;
-`
+`;
 const IconWrap = styled.span`
   margin-right: 6px;
   height: 38px;
-`
+`;
 
 const DynamicBoldText = styled.span`
   font-size: 24px;
@@ -170,11 +181,11 @@ const DynamicBoldText = styled.span`
   margin-right: 6px;
   height: 38px;
   display: inline-block;
-`
+`;
 
 const GeneralText = styled.span`
   width: 230px;
-`
+`;
 
 const InfoSentence = styled.p`
   display: flex;
@@ -184,7 +195,7 @@ const InfoSentence = styled.p`
   text-align: left;
   line-height: 22px;
   margin-bottom: 22px;
-`
+`;
 
 const SubText = styled.div`
   position: absolute;
@@ -195,20 +206,20 @@ const SubText = styled.div`
   line-height: 14px;
   text-align: center;
   letter-spacing: 0.05em;
-`
+`;
 
 const ToggleClosedButton = styled.div`
   position: absolute;
   right: 6px;
   top: 12px;
-`
+`;
 
 const CloseButton = styled.button`
   position: absolute;
   width: 115px;
   height: 32px;
   top: 268px;
-  background: #A8192E;
+  background: #a8192e;
   border: 0px solid black;
   border-radius: 50px;
   color: white;
@@ -217,4 +228,4 @@ const CloseButton = styled.button`
   letter-spacing: 0.15em;
   line-height: 15px;
   text-align: center;
-`
+`;
