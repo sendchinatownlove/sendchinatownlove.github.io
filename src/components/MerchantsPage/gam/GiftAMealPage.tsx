62422cab098096aebe0461b0538afb79cfe32ef9
Chris Ng <chris.ng93@gmail.com>
2020-11-16 23:12:24 -0800

    scaffold done

--- src/components/MerchantsPage/gam/GiftAMealPage.tsx
+++ src/components/MerchantsPage/gam/GiftAMealPage.tsx
@@ -1,12 +1,16 @@
 import React, { useEffect, useState } from 'react';
-import styles from './styles.module.scss';
+import { useTranslation } from 'react-i18next';
+
 import { getCampaigns } from '../../../utilities/api';
+import type { Campaign } from '../../../utilities/api';
 import illustrated_flatlay_hero from '../images/illustrated_flatlay_hero.png';
 import CampaignInstructions from './CampaignInstructions';
-import NoActiveCampaignsBox from './NoCampaignsBox';
 import CampaignListItem from './CampaignListItem';
+import MegaGamListItem from './MegaGamListItem';
+import NoActiveCampaignsBox from './NoCampaignsBox';
 import VideoComponent from './VideoComponent';
-import { useTranslation } from 'react-i18next';
+
+import styles from './styles.module.scss';
 
 interface Props {
   menuOpen: boolean;
@@ -15,20 +19,34 @@ interface Props {
 const GiftAMealPage = (props: Props) => {
   const { t } = useTranslation();
 
-  const [activeCampaigns, setActiveCampaigns] = useState([]);
-  const [pastCampaigns, setPastCampaigns] = useState([]);
-  const [selectedCampaign, setSelectedCampaign] = useState(null);
+  const [activeCampaigns, setActiveCampaigns] = useState<Campaign[]>([]);
+  const [pastCampaigns, setPastCampaigns] = useState<Campaign[]>([]);
+  const [selectedCampaignId, setSelectedCampaignId] = useState<number | null>(
+    null
+  );
 
   const fetchData = async () => {
-    const campaignData = await getCampaigns();
-    const active = campaignData.data.filter(
-      (campaign: any) => campaign.active && campaign.valid
-    );
-    setActiveCampaigns(active);
-    const past = campaignData.data
-      .filter((campaign: any) => !campaign.active)
-      .reverse();
-    setPastCampaigns(past);
+    const campaignResponse = await getCampaigns();
+    const activeMegaGam: Campaign[] = [];
+    const active: Campaign[] = [];
+    const past: Campaign[] = [];
+
+    // Campaigns come back sorted by oldest to newest end date.
+    campaignResponse.data.forEach((campaign: Campaign) => {
+      if (campaign.project_id) {
+        activeMegaGam.push(campaign);
+      } else if (campaign.active && campaign.valid) {
+        active.push(campaign);
+      } else {
+        past.push(campaign);
+      }
+    });
+
+    // We want active Mega GAM campaigns to show up first and then active
+    // campaigns from oldest to newest.
+    setActiveCampaigns(activeMegaGam.concat(active));
+    // Inactive campaigns should show up newest to oldest.
+    setPastCampaigns(past.reverse());
   };
 
   useEffect(() => {
@@ -65,17 +83,18 @@ const GiftAMealPage = (props: Props) => {
       </button>
       {activeCampaigns.length ? (
         <>
-          <h5 className={styles.campaignHeader}>
-            {t('gamHome.activeSection')}
-          </h5>
-          {activeCampaigns.map((campaign: any) => (
-            <CampaignListItem
-              campaign={campaign}
-              key={campaign.id}
-              selectedCampaign={selectedCampaign}
-              setSelectedCampaign={setSelectedCampaign}
-            />
-          ))}
+          {activeCampaigns.map((campaign: Campaign) =>
+            campaign.project_id ? (
+              <MegaGamListItem campaign={campaign} key={campaign.id} />
+            ) : (
+              <CampaignListItem
+                campaign={campaign}
+                key={campaign.id}
+                selectedCampaignId={selectedCampaignId}
+                setSelectedCampaignId={setSelectedCampaignId}
+              />
+            )
+          )}
         </>
       ) : (
         <NoActiveCampaignsBox />
@@ -89,12 +108,12 @@ const GiftAMealPage = (props: Props) => {
       </div>
 
       <h5 className={styles.campaignHeader}>{t('gamHome.pastSection')}</h5>
-      {pastCampaigns.map((campaign: any) => (
+      {pastCampaigns.map((campaign: Campaign) => (
         <CampaignListItem
           campaign={campaign}
           key={campaign.id}
-          selectedCampaign={selectedCampaign}
-          setSelectedCampaign={setSelectedCampaign}
+          selectedCampaignId={selectedCampaignId}
+          setSelectedCampaignId={setSelectedCampaignId}
         />
       ))}
     </div>
---
+++ src/components/MerchantsPage/gam/MegaGamListItem.tsx
@@ -0,0 +1,150 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import styled from 'styled-components';
+
+import { Campaign } from '../../../utilities/api/types';
+import { tabletScreens } from '../../../utilities/general/responsive';
+
+interface Props {
+  campaign: Campaign;
+}
+
+const MegaGamListItem = ({ campaign }: Props) => {
+  const { t } = useTranslation();
+  return (
+    <Container>
+      <HeroImage heroImageUrl={campaign.gallery_image_urls[0]} />
+      <Content>
+        {/* TODO: Figure out if this is the correct header. If so, we need to translate. */}
+        <Header>Mega-GAM</Header>
+        {/* TODO: Don't hard code this. Figure out where we should get this data from. */}
+        <Subheader>December Campaign</Subheader>
+        <Description>{campaign.description}</Description>
+        <DonationContainer>
+          {/* TODO: Add donation data UI. */}
+          <DonationContent>_DONATION_DATA_</DonationContent>
+          {campaign.active && (
+            // TODO: Open payment modal.
+            <Button className="button--filled" onClick={undefined}>
+              {t('gamHome.listItem.giftButton')}
+            </Button>
+          )}
+        </DonationContainer>
+        <SellerDistributorContent>
+          {/* TODO: Translate. */}
+          <LearnMoreText>
+            Learn more about our participating merchants:
+          </LearnMoreText>
+          {/* TODO: Add seller/distributor pairs. */}
+          <SellerDistributorPairs>
+            _SELLER_DISTRIBUTOR_PAIRS_
+          </SellerDistributorPairs>
+        </SellerDistributorContent>
+      </Content>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  background-color: #f7f7f7;
+  display: flex;
+  flex-direction: column;
+`;
+
+// TODO: Figure out how this image should scale.
+const HeroImage = styled.div`
+  background-image: url('${(props: { heroImageUrl: string }) =>
+    props.heroImageUrl}');
+  background-size: cover;
+  height: 252px;
+  width: 100%;
+
+  @media (${tabletScreens}) {
+    height: 106px;
+  }
+`;
+
+const Content = styled.div`
+  padding: 44px 66px;
+
+  @media (${tabletScreens}) {
+    padding: 16px 36px;
+  }
+`;
+
+const Header = styled.div`
+  color: #1e7c9a;
+  font-size: 20px;
+  font-weight: 600;
+  letter-spacing: 0.05em;
+  margin-bottom: 16px;
+
+  @media (${tabletScreens}) {
+    margin-bottom: 8px;
+  }
+`;
+
+const Subheader = styled.div`
+  font-size: 24px;
+  font-weight: 600;
+  letter-spacing: 0.05em;
+  margin-bottom: 36px;
+
+  @media (${tabletScreens}) {
+    margin-bottom: 16px;
+  }
+`;
+
+const Description = styled.div`
+  margin-bottom: 24px;
+`;
+
+const DonationContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 60px;
+
+  @media (${tabletScreens}) {
+    flex-direction: column;
+  }
+`;
+
+const DonationContent = styled.div`
+  @media (${tabletScreens}) {
+    margin-bottom: 28px;
+  }
+`;
+
+const Button = styled.div`
+  text-align: center;
+  width: 240px;
+  letter-spacing: 0.15em;
+  cursor: pointer;
+  margin-bottom: 18px;
+  font-family: 'Open Sans', 'Helvetica Neue', sans-serif;
+  font-weight: 700;
+  font-size: 16px;
+  letter-spacing: 0.05em;
+
+  @media (${tabletScreens}) {
+    font-size: 14px;
+    margin-bottom: 16px;
+    width: 100%;
+  }
+`;
+
+const SellerDistributorContent = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const LearnMoreText = styled.div`
+  margin-bottom: 36px;
+`;
+
+const SellerDistributorPairs = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export default MegaGamListItem;
