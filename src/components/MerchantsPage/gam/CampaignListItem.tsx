2d13bf37da3283ebad30c95cb3f78faffa775af6
Alice Luo <squeeshed@gmail.com>
2020-11-20 09:16:00 -0600

    use seller_distributor_pairs for gam

--- src/components/MerchantsPage/gam/CampaignListItem.tsx
+++ src/components/MerchantsPage/gam/CampaignListItem.tsx
@@ -37,7 +37,9 @@ const CampaignListItem = (props: Props) => {
 
   const fetchData = async () => {
     const distributorData = await getDistributor(campaign.distributor_id);
-    const merchantData = await getSeller(campaign.seller_id);
+    const seller_id = campaign.seller_distributor_pairs[0].seller_id;
+    const merchantData = await getSeller(seller_id);
+
     setDistributor(distributorData.data);
     setMerchant(merchantData.data);
     if (campaign.nonprofit_id) {
