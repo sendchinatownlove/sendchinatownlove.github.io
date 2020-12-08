5c82f0daeaa70f2955f1af40b3cfebcca930cece
Wilson <wilsonj806@gmail.com>
2020-12-07 22:04:38 -0500

    Merge changes from dev

--- src/components/MerchantsPage/gam/CampaignProgressBar.tsx
+++ src/components/MerchantsPage/gam/CampaignProgressBar.tsx
@@ -1,7 +1,7 @@
 import moment from 'moment';
 import React from 'react';
 
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import styled from 'styled-components';
 
 import { tabletScreens } from '../../../utilities/general/responsive';
@@ -48,6 +48,7 @@ const CampaignProgressBar = ({
     return Math.round((raised / target) * 100);
   };
   const percentRaised = calculatePercentRaised(totalRaised, targetAmount);
+  const remainingDays = Math.max(moment(endDate).diff(moment(), 'days'), 0);
 
   return (
     <Container>
@@ -73,10 +74,9 @@ const CampaignProgressBar = ({
         {isActive && (
           <EndsAtContainer isModal={isModal}>
             {t('buyMeal.endsIn')}{' '}
-            <ProgressText color={PROGRESS_BAR_COLOR}>
-              {moment(endDate).diff(moment(), 'days')}
-            </ProgressText>{' '}
-            {t('buyMeal.days')}
+            <Trans i18nKey="buyMeal.dayWithCount" count={remainingDays}>
+              <ProgressText color={PROGRESS_BAR_COLOR} />
+            </Trans>
           </EndsAtContainer>
         )}
       </ProgressTextContainer>
