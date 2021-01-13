ef8170ea4e7aa8ebb5ce60fde02670d1a6814372
jacoblb64 <jacoblb64@users.noreply.github.com>
2021-01-13 19:21:35 +0000

    deploy: 651d277d217d7f0eb22d34c6c067a566dace97a2

---
+++ precache-manifest.df8d742b2705102c91e38d26074a2b13.js
@@ -0,0 +1,266 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee02f53c9856587f9bd48d7edb338b5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c31f5948fd550382ff41",
+    "url": "/static/css/15.c51f89df.chunk.css"
+  },
+  {
+    "revision": "067aef54633d1c7e0af1",
+    "url": "/static/css/16.ba88347b.chunk.css"
+  },
+  {
+    "revision": "a5901af3807be128eef3",
+    "url": "/static/css/6.737cbb83.chunk.css"
+  },
+  {
+    "revision": "f93917469fa309b4e9ad",
+    "url": "/static/css/7.91c4fac3.chunk.css"
+  },
+  {
+    "revision": "1bf40254d99baffd1108",
+    "url": "/static/css/9.949eb22e.chunk.css"
+  },
+  {
+    "revision": "93ad95730e45a0260b94",
+    "url": "/static/css/main.d3c53cd2.chunk.css"
+  },
+  {
+    "revision": "5f498b03dd44d5e4f218",
+    "url": "/static/js/0.98842761.chunk.js"
+  },
+  {
+    "revision": "b99a9d2eab36e284f897",
+    "url": "/static/js/1.346dfdf8.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/1.346dfdf8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30972a1bff8cc372a58c",
+    "url": "/static/js/10.3431480d.chunk.js"
+  },
+  {
+    "revision": "86029f708f3d8a42e55d",
+    "url": "/static/js/11.0cf92428.chunk.js"
+  },
+  {
+    "revision": "32ec626dac62da947ed9",
+    "url": "/static/js/12.57262405.chunk.js"
+  },
+  {
+    "revision": "7b7b92fc28bacf722522",
+    "url": "/static/js/13.a2d964fa.chunk.js"
+  },
+  {
+    "revision": "f087e520078b7bb6c840",
+    "url": "/static/js/14.3e51c16e.chunk.js"
+  },
+  {
+    "revision": "c31f5948fd550382ff41",
+    "url": "/static/js/15.3ae7aaac.chunk.js"
+  },
+  {
+    "revision": "067aef54633d1c7e0af1",
+    "url": "/static/js/16.38c68945.chunk.js"
+  },
+  {
+    "revision": "81896c98bac7b5b16ab1d3790da5b937",
+    "url": "/static/js/16.38c68945.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcbc6d1a973ed3960a1e",
+    "url": "/static/js/17.baf3f8d7.chunk.js"
+  },
+  {
+    "revision": "82d163184dee9f772e84",
+    "url": "/static/js/18.83c19bc1.chunk.js"
+  },
+  {
+    "revision": "da82c9ef8348a6c0ab11",
+    "url": "/static/js/2.3f0cc737.chunk.js"
+  },
+  {
+    "revision": "fd7525d544dd9c67d07855cb8778e590",
+    "url": "/static/js/2.3f0cc737.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b128a3aa9f1348fe001",
+    "url": "/static/js/3.57d2efd2.chunk.js"
+  },
+  {
+    "revision": "a5901af3807be128eef3",
+    "url": "/static/js/6.0a71131a.chunk.js"
+  },
+  {
+    "revision": "905f7156df7745c410047a82bdf10c80",
+    "url": "/static/js/6.0a71131a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f93917469fa309b4e9ad",
+    "url": "/static/js/7.66ff6529.chunk.js"
+  },
+  {
+    "revision": "0a9ccc47b8721e90a5f0d5046e777199",
+    "url": "/static/js/7.66ff6529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa59d0f3043a22fead79",
+    "url": "/static/js/8.efc50e96.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/8.efc50e96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bf40254d99baffd1108",
+    "url": "/static/js/9.b93e789d.chunk.js"
+  },
+  {
+    "revision": "275fe79abee3b697f1673c8bd9c58856",
+    "url": "/static/js/9.b93e789d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93ad95730e45a0260b94",
+    "url": "/static/js/main.0ab08e78.chunk.js"
+  },
+  {
+    "revision": "42e21da29406497f6624",
+    "url": "/static/js/runtime-main.6cd2cbd0.js"
+  },
+  {
+    "revision": "f72ee01693c673c09463dfdaa57b3652",
+    "url": "/static/media/404-error-image.f72ee016.png"
+  },
+  {
+    "revision": "0fc95748a279fa25237e1eadcc2e0db8",
+    "url": "/static/media/CircleLogo.0fc95748.png"
+  },
+  {
+    "revision": "79d9382b078a76e48361ebdd247b9df2",
+    "url": "/static/media/CrawlMap.79d9382b.png"
+  },
+  {
+    "revision": "dbad4a539c633517194c84e4d4cfcd97",
+    "url": "/static/media/ExternalLink.dbad4a53.svg"
+  },
+  {
+    "revision": "f2f024bae541eb9e2b61f0a15413f69a",
+    "url": "/static/media/LogoTextDown.f2f024ba.png"
+  },
+  {
+    "revision": "fb69d2d43c22dec57619502b207b8ef0",
+    "url": "/static/media/PassportDashboardBackground.fb69d2d4.png"
+  },
+  {
+    "revision": "73de5a33779ac0c6451b202c015beea1",
+    "url": "/static/media/PureLogoNoBorder.73de5a33.png"
+  },
+  {
+    "revision": "53dab2f2751e3398aeea9e1e7d8b4842",
+    "url": "/static/media/VoucherBack.53dab2f2.png"
+  },
+  {
+    "revision": "17e749a1a28379f3e06aaa11cb557178",
+    "url": "/static/media/VoucherFront.17e749a1.png"
+  },
+  {
+    "revision": "9ad02a20270492477468dc9e11cb2885",
+    "url": "/static/media/campaign_default.9ad02a20.png"
+  },
+  {
+    "revision": "041d975f97c138b469eecafffc701125",
+    "url": "/static/media/chevron.041d975f.svg"
+  },
+  {
+    "revision": "f9e716bafd5bcd4ef121111701cb48c5",
+    "url": "/static/media/chinatown-logo.f9e716ba.png"
+  },
+  {
+    "revision": "df5a5640842d575704049896a3f60e13",
+    "url": "/static/media/citi_bike.df5a5640.png"
+  },
+  {
+    "revision": "88b799521a21c98636590c1b9092bb0b",
+    "url": "/static/media/cost-breakdown.88b79952.png"
+  },
+  {
+    "revision": "46412c79a0082e73eb711911bf68237d",
+    "url": "/static/media/female.46412c79.svg"
+  },
+  {
+    "revision": "c58f05a43debc214e4c077c1e2c7e1e5",
+    "url": "/static/media/gam_icon_step1.c58f05a4.svg"
+  },
+  {
+    "revision": "9a0ca5e87d66e748175f5ec471526ae7",
+    "url": "/static/media/gam_icon_step2.9a0ca5e8.svg"
+  },
+  {
+    "revision": "ad0c5291e0cf11a108a2a25585d361df",
+    "url": "/static/media/gam_icon_step3.ad0c5291.svg"
+  },
+  {
+    "revision": "b9832c02b38d7b04b36944424aead171",
+    "url": "/static/media/gam_icon_step4.b9832c02.svg"
+  },
+  {
+    "revision": "050015e1de2281091de80db9be0f9e32",
+    "url": "/static/media/illustrated_flatlay_hero.050015e1.png"
+  },
+  {
+    "revision": "da139363fdb3163f5566d3ef64df326b",
+    "url": "/static/media/lantern-footer.da139363.png"
+  },
+  {
+    "revision": "0e901363cce1dbfaa2832ea5b21f26aa",
+    "url": "/static/media/lantern-hero-top.0e901363.png"
+  },
+  {
+    "revision": "909f1479b519e732b5a828cc5a69317a",
+    "url": "/static/media/logo.909f1479.svg"
+  },
+  {
+    "revision": "7e1a70b6774c5a168940bee9068ea1a0",
+    "url": "/static/media/misc-store.7e1a70b6.png"
+  },
+  {
+    "revision": "631d2c4cb5697f0bedd9b501a38ec249",
+    "url": "/static/media/nyc-background.631d2c4c.png"
+  },
+  {
+    "revision": "701fefa015eaef76ef9f970091650e1f",
+    "url": "/static/media/partners-46.701fefa0.png"
+  },
+  {
+    "revision": "6f4df769c7647672e3f96e221dfc31e7",
+    "url": "/static/media/partners-ccba.6f4df769.png"
+  },
+  {
+    "revision": "77cf4f37e1ca0e2511dcb80942fa9ebd",
+    "url": "/static/media/partners-cccny.77cf4f37.png"
+  },
+  {
+    "revision": "f53a7c5edc316526409ecebca6c11a12",
+    "url": "/static/media/partners-nyccc.f53a7c5e.png"
+  },
+  {
+    "revision": "b9c4a3c5bf3cdcc1082520c269bea9f5",
+    "url": "/static/media/partners-prm.b9c4a3c5.png"
+  },
+  {
+    "revision": "64e3d12ba0f9e6bcae6263e81b858eab",
+    "url": "/static/media/partners-thinkct.64e3d12b.png"
+  },
+  {
+    "revision": "aabb5f71ceb166639706cd9e912ff7ca",
+    "url": "/static/media/partners-udooda.aabb5f71.png"
+  },
+  {
+    "revision": "e16097f43a3b4d419fdac49029b54e9e",
+    "url": "/static/media/passportIcon.e16097f4.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d185c15e6418f6236dc0948f50b4b93e.js"
+  "/precache-manifest.df8d742b2705102c91e38d26074a2b13.js"
 );
 
 self.addEventListener('message', (event) => {
