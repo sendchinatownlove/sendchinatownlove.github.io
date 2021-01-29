c62b6d15d1642ee7c42df8b95dae19960255b8be
abraham villaroman <bvillaroman@gmail.com>
2021-01-29 18:27:39 -0500

    quick fix

--- src/pages/PassportRedemption/Passport/index.tsx
+++ src/pages/PassportRedemption/Passport/index.tsx
@@ -128,7 +128,7 @@ const Passport = (props: Props) => {
     setShowPopup(false);
     localStorage.setItem(
       'amountOfReceipts',
-      ((receipts.length % 3) + 1).toString()
+      ((receipts.length / 3) + 1).toString()
     );
   };
 
@@ -137,7 +137,7 @@ const Passport = (props: Props) => {
     props.setCurrentScreenView(ScreenType.Rewards);
     localStorage.setItem(
       'amountOfReceipts',
-      ((receipts.length % 3) + 1).toString()
+      ((receipts.length / 3) + 1).toString()
     );
   };
 
