0cb95e5e0401378de648db2a78b4ca30bdde4c02
Wilson <wilsonj806@gmail.com>
2021-02-28 20:54:40 -0500

    Add sortable table with some styling

--- src/pages/DistributorTools/DistributorDashboard.tsx
+++ src/pages/DistributorTools/DistributorDashboard.tsx
@@ -7,36 +7,83 @@ import SimplePager from '../../components/SimplePager/SimplePager';
 import { PageCountContext } from '../../utilities/general/PageCountContext';
 import { getAllVouchers } from '../../utilities/api/interactionManager';
 
+import Table, { FIELDS } from '../../components/SortableTable/Table';
+
 const Dashboard = () => {
   const { setPageCount } = useContext(PageCountContext);
   const { search } = useLocation();
+  const params = new URLSearchParams(search);
+  const pageNo = params.get('page');
+
   const [shouldRedirect, setShouldRedirect] = useState(false);
+  const [data, setData] = useState<any[] | undefined>(undefined);
+  const [sellersList, setSellersList] = useState<{ [key: string]: any }>({});
 
   useEffect(() => {
     const asyncFetch = async () => {
       // If URL has page number, use that, else use currPage
-      const params = new URLSearchParams(search);
-      const pageNo = params.get('page');
-      const res = await getAllVouchers(pageNo && pageNo !== '1' ? pageNo : '1');
-      if (res.status === 401) return setShouldRedirect(true);
-      setPageCount(res.headers['total-pages']);
+      const {
+        status,
+        data: { gift_cards, seller_names },
+        headers,
+      } = await getAllVouchers(pageNo && pageNo !== '1' ? pageNo : '1');
+      if (status === 401) return setShouldRedirect(true);
+      setPageCount(headers['total-pages']);
+      setData(gift_cards);
+      setSellersList(seller_names);
     };
+    if (data) return;
     asyncFetch();
-  }, [search, setPageCount]);
+  }, [pageNo, setPageCount, data]);
+
+  const sortData = (ind, isAsc) =>
+    setData(sortByColumn(data, FIELDS)(ind, isAsc));
 
   return (
     <Main>
       {shouldRedirect && <Redirect to={'/distributor/login'} />}
       <Header>
         <Heading>Distributor Dashboard: Gift-a-Meal Tracker</Heading>
-        <Heading>礼品券记录[COPY TO UPDATE]</Heading>
+        <Heading>礼品券记录</Heading>
       </Header>
-      <Section>
+      <DistributorOverview>
+        <MetadataHeading num={1}>
+          # Active Vouchers 可使用的礼品券数量
+        </MetadataHeading>
+        <MetadataHeading num={2}>Total balance 总结余</MetadataHeading>
+        <MetadataHeading num={3}>Last Updated 上次更新时间</MetadataHeading>
+        <Metadata num={1}>yes</Metadata>
+        <Metadata num={2}>A lot</Metadata>
+        <Metadata num={3}>At some point</Metadata>
+      </DistributorOverview>
+      <TableContainer>
+        <Table
+          fields={FIELDS}
+          data={data || []}
+          sortFn={sortData}
+          sellersList={sellersList}
+        />
+        {data ? null : 'No Vouchers To Load'}
         <SimplePager />
-      </Section>
+      </TableContainer>
       <Footer />
     </Main>
   );
+};
+
+const sortByColumn = (data, fields) => (fieldIndex, isAsc) => {
+  const res = [...data].sort((a, b) => {
+    const fieldName = fields[fieldIndex].name;
+    if (a[fieldName] > b[fieldName]) {
+      return isAsc ? -1 : 1;
+    } else if (a[fieldName] < b[fieldName]) {
+      return isAsc ? 1 : -1;
+    } else {
+      return 0;
+    }
+  });
+  console.log(res);
+  return res;
 };
 
 export default Dashboard;
@@ -48,17 +95,50 @@ const Main = styled.main`
 
 const Header = styled.header`
   width: 100%;
-  height: 218px;
+  height: 246px;
   background: rgb(168, 25, 46);
-  padding: 82px 0 0 129px;
+  padding: 82px 0 0 120px;
   box-sizing: border-box;
 `;
 
 const Heading = styled.h1`
   color: white;
-  font-size: 24px;
+  font-size: 32px;
+  width: 672px;
+  font-weight: 700;
+  letter-spacing: -0.01em;
+  line-height: 46px;
+  margin: 0;
 `;
 
-const Section = styled.section`
+const TableContainer = styled.article`
   margin: 0 130px 80px;
+`;
+
+const DistributorOverview = styled.section`
+  display: grid;
+  background: rgba(171, 25, 46, 0.07);
+  width: 100%;
+  height: 160px;
+  padding: 48px;
+  grid-template-rows: 25px 1fr;
+  grid-teplate-columns: 1fr 1fr 1fr;
+  grid-template-areas:
+    'heading1 heading2 heading3'
+    'val1 val2 val3';
+  justify-content: center;
+  grid-gap: 7px 192px;
+  margin-bottom: 80px;
+`;
+const MetadataHeading = styled.h2`
+  ${({ num }: { num: number }) => `grid-area: heading${num} ;`}
+  font-family: Noto Sans SC, sans-serif;
+  text-transform: uppercase;
+  font-size: 16px;
+`;
+const Metadata = styled.p`
+  ${({ num }: { num: number }) => `grid-area: val${num} ;`}
+  text-transform: uppercase;
+  font-size: 24px;
+  font-weight: 700;
 `;
