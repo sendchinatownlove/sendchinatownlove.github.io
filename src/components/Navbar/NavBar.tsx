64f5c9940e1abec24dafebbd4b7970d5daf0ac5e
stanley zheng <stanley@zheng.nyc>
2021-05-25 12:00:34 -0400

    updating zine text

--- src/components/Navbar/NavBar.tsx
+++ src/components/Navbar/NavBar.tsx
@@ -127,9 +127,9 @@ const NavBar = (props: Props) => {
         {dropdownOpen && <DropdownMobile>{drop}</DropdownMobile>}
         <NavLink
           compact={hamburgerOpen.toString()}
-          href="https://www.sendchinatownlove.com/apahm-gam"
-          i18nText="APAHM-GAM"
-          altText="APAHM-GAM"
+          href="https://www.sendchinatownlove.com/send-chinatown-love-letters.html"
+          i18nText="Send Chinatown Love Letters"
+          altText="Send Chinatown Love Letters"
         />
         <NavLink
           compact={hamburgerOpen.toString()}
